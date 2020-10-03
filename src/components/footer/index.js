a13ad15a844029ded939d779418f9b38c3029ed5
feduardomelo <emelo174@gmail.com>
2020-10-03 17:36:39 -0300

    alterando footer

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -4,21 +4,24 @@ import './style.css'
 const Footer = () => {
     return(
     
-    <footer id="footer">
-        <div className="dados">
-            <img src={require("../img/eduardo.jpeg")}  className="perfil" alt=""/>
-            <h1>Fábio Eduardo</h1>
-            <div className="social">
-                <a href="https://github.com/feduardomelo" target="_blank"><img src= {require("../img/logo_github.svg")} alt="" /></a>
-                <a href="https://www.linkedin.com/in/eduardo-melo-96b3b1154/" target="_blank"><img src= {require("../img/logo_linkedin.png")} alt="" /></a>
+    <footer>
+        <h2>Idealizadores</h2>
+        <div id="footer">
+            <div className="dados">
+                <img src={require("../img/eduardo.jpeg")}  className="perfil" alt=""/>
+                <h1>Fábio Eduardo</h1>
+                <div className="social">
+                    <a href="https://github.com/feduardomelo" target="_blank"><img src= {require("../img/logo_github.svg")} alt="" /></a>
+                    <a href="https://www.linkedin.com/in/eduardo-melo-96b3b1154/" target="_blank"><img src= {require("../img/logo_linkedin.png")} alt="" /></a>
+                </div>
             </div>
-        </div>
-        <div className="dados">
-            <img src={require("../img/estela.jpeg")} className="perfil" alt=""/>
-            <h1>Maria Estela</h1>
-            <div className="social">
-                <a href="https://github.com/estelasouza" target="_blank"><img src= {require("../img/logo_github.svg")} alt=""/></a>
-                <a href="https://www.linkedin.com/in/maria-estela-souza-737ab216a/" target="_blank"><img src= {require("../img/logo_linkedin.png")} alt=""/></a>
+            <div className="dados">
+                <img src={require("../img/estela.jpeg")} className="perfil" alt=""/>
+                <h1>Maria Estela</h1>
+                <div className="social">
+                    <a href="https://github.com/estelasouza" target="_blank"><img src= {require("../img/logo_github.svg")} alt=""/></a>
+                    <a href="https://www.linkedin.com/in/maria-estela-souza-737ab216a/" target="_blank"><img src= {require("../img/logo_linkedin.png")} alt=""/></a>
+                </div>
             </div>
         </div>
     </footer>
