924bed109d529e476836a991624d21c04168728a
feduardomelo <emelo174@gmail.com>
2020-10-04 09:57:58 -0300

    alteraçoes com bootstrap

--- src/pages/colaborador/index.js
+++ src/pages/colaborador/index.js
@@ -1,32 +1,66 @@
 import React from 'react'
 import './style.css'
 
+
 const Colaborador = () => {
     return(
-    <div className="content">
+    <div className="content container">
         <title>Colaborador</title>
-        <label>Nome da empresa</label>
-        <select name="name_empresa" id="name_empresa">
-            <option value="1">Option 1</option>
-            <option value="2">Option 2</option>
-            <option value="3">Option 3</option>
-            <option value="0">OUTRA</option>
-        </select>
-    
-        <label>Cargo</label>
-        <select name="name_empresa" id="name_empresa">
-            <option value="1">Option 1</option>
-            <option value="2">Option 2</option>
-            <option value="3">Option 3</option>
-            <option value="0">OUTRA</option>
-        </select>
+        <form>
+            <label>Nome da empresa</label>
+            <select className="form-control form-control-lg" name="name_empresa" id="name_empresa">
+                <option value="1">Option 1</option>
+                <option value="2">Option 2</option>
+                <option value="3">Option 3</option>
+                <option value="0">OUTRA</option>
+            </select>
         
-        <label>Salário</label>
-        <input placeholder="Valor em R$" type="number"/>
-        
-        <button type="submit">Submeter</button>
+            <label>Cargo</label>
+            <select className="form-control form-control-lg"name="name_empresa" id="name_empresa">
+                <option value="1">Option 1</option>
+                <option value="2">Option 2</option>
+                <option value="3">Option 3</option>
+                <option value="0">OUTRA</option>
+            </select>
+
+            <span>Salário</span>
+            <div className="input-group mb-3">
+                <div className="input-group mb-3">
+                    <div className="input-group-prepend">
+                        <span className="input-group-text">R$</span>
+                    </div>
+                    <input type="number" className="form-control" aria-label="Amount (to the nearest dollar)"/>
+                    <div className="input-group-append">
+                        <span className="input-group-text">.00</span>
+                    </div>
+                </div>
+            </div>
+
+            <div>
+                <label>De 0 a 5, qual nota você dá para o ambiente de trabalho nessa empresa?</label>
+                <div className="input-group">
+                    <div className="input-group-prepend">
+                        <div className="input-group-text">
+                            <input type="radio" aria-label="1" name="nota"/>
+                            <label for="1">1</label>
+                            <input type="radio" aria-label="2" name="nota"/>
+                            <label for="2">2</label>
+                            <input type="radio" aria-label="3" name="nota"/>
+                            <label for="3">3</label>
+                            <input type="radio" aria-label="4" name="nota"/>
+                            <label for="4">4</label>
+                            <input type="radio" aria-label="5" name="nota"/>
+                            <label for="5">5</label>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <a href="/empresas" className="btn" type="submit">Submeter</a>
+        </form>
     
     </div>
 )}
+
+
 
 export default Colaborador
--- src/pages/empresas/index.js
+++ src/pages/empresas/index.js
@@ -6,8 +6,8 @@ const Empresas = () => {
         <div>
             <title>Empresas</title>
             <header>
-                <label >O que você procura?</label>
-                <select name="opcoes" id="filtro">
+                <label >Qual empresa você procura?</label>
+                <select className="form-control form-control-lg" name="opcoes" id="filtro">
                     <option value="0">Filtro</option>
                     <option value="Ambiente de Trabalho">Ambiente de Trabalho</option>
                     <option value="Salário">Salário</option>
@@ -18,44 +18,29 @@ const Empresas = () => {
             <div className="container" id="blocos">
                 <div className="empresas">
                     <h1>Empresa 1</h1>
-                    <span>Tecnologias utilizadas</span>
-                    <ul>
-                        <li>Another list item</li>
-                        <li>Yup, another list item</li>
-                        <li>Another list item</li>
-                    </ul>
+                    
                     <p>Cargo:</p>
                     <p>Valor do salário:</p>
                     <p>Total de análises:</p>
-                    <p>Ambiente de trabalho</p>
+                    <p>Ambiente de trabalho:</p>
                 </div>
 
                 <div className="empresas">
-                    <h1>Empresa 2</h1>
-                    <span>Tecnologias utilizadas</span>
-                    <ul>
-                        <li>Another list item</li>
-                        <li>Yup, another list item</li>
-                        <li>Another list item</li>
-                    </ul>
+                    <h1>Empresa 1</h1>
+                    
                     <p>Cargo:</p>
                     <p>Valor do salário:</p>
                     <p>Total de análises:</p>
-                    <p>Ambiente de trabalho</p>
+                    <p>Ambiente de trabalho:</p>
                 </div>
 
                 <div className="empresas">
-                    <h1>Empresa 3</h1>
-                    <span>Tecnologias utilizadas</span>
-                    <ul>
-                        <li>Another list item</li>
-                        <li>Yup, another list item</li>
-                        <li>Another list item</li>
-                    </ul>
+                    <h1>Empresa 1</h1>
+                    
                     <p>Cargo:</p>
                     <p>Valor do salário:</p>
                     <p>Total de análises:</p>
-                    <p>Ambiente de trabalho</p>
+                    <p>Ambiente de trabalho:</p>
                 </div>
                 
             </div>
