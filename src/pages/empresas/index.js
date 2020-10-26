2cd783caacd9ddee42d5a66b244a40073faa7e12
estelasouza <mecls@cin.ufpe.br>
2020-10-26 08:29:40 -0300

    componentizando index empresa

--- src/pages/empresas/index.js
+++ src/pages/empresas/index.js
@@ -1,31 +1,39 @@
 
-import React, {useState} from 'react'
+import React, {Component} from 'react'
 import './style.css'
 import { useHistory } from "react-router-dom"
 import EmpresaService from "../../services/empresa"
 
-export default function Empresas(){
-    const history = useHistory()
-    
-    const [empresas, setEmpresas] = useState([])
-    
-    EmpresaService.getEmpresa().then(response => {
-        setEmpresas(response.data)
-    })
+class Empresas extends Component{
+    state = {
+        empresas: []
+    }
+
+    async componentDidMount(){
+        const empresasTotal = await EmpresaService.getEmpresa()
+        this.setState({ empresas: empresasTotal.data})
+    }
+
+    render(){
+        const { empresas } = this.state
+        console.log(empresas )
+        const id = 1
+        const avaliacao = empresas[id] || []
+        console.log(avaliacao)
+        // const history = useHistory()
+
+
 
     return(
-        <div >
-            <title>Empresas</title>
+        <div>
+        <title>Empresas</title>
             <header className="container">
                 <div className="col">
                 <label ><h3>Qual empresa você procura? </h3></label>
                 </div>
                 <div className="col">
 
-                <select onChange={(event) => {
-                    
-                    history.push(`empresa/${event.target.value}`)
-                }} className="form-control form-control-lg" name="opcoes" id="filtro">
+                <select onChange='' className="form-control form-control-lg" name="opcoes" id="filtro">
                     <option>Selecione a empresa</option>
                     {empresas.map(empresa => (
                         <option value={empresa.id} >{empresa.nome_empresa}</option>
@@ -38,7 +46,7 @@ export default function Empresas(){
                 <div className="row ">
                 
                 {empresas.map(empresa => (
-                    empresa.avaliacao.map(avaliacao => (
+                    
                         <div className=" col-4 padding " id="blocos">
 
         <div className="card card-body color ">
@@ -52,11 +60,12 @@ export default function Empresas(){
                         </div>
                 </div>
 
-                    ))
-                ))}
+                    )
+                )}
             
             </div>
         </div>
-)}
+        )}}
+export default Empresas
 
 
